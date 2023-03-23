11ca7153c3b3f44feeb2219f6e668cb7facfc45d
Ergin Dapaj <ergindapaj081@gmail.com>
2023-03-23 18:36:58 +0100

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
     
     <main className={styles.main}>
       <Header />
-      {/* <ContentTemp /> */}
-      <Content />
+      <ContentTemp />
+      {/* <Content /> */}
     </main>
   )
 }
