b3d7c543e1153e842ddb106da29f91c4db78ed95
ErginDapaj <ergindapaj081@gmail.com>
2023-04-06 13:37:32 +0200

    removed the cache from the lanyard, i need to add cache on my end now

--- src/app/api/discord/route.ts
+++ src/app/api/discord/route.ts
@@ -1,21 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
+
 export async function GET(req: Request, res: Response) {
+  // Fetch data from another API endpoint using await
+  const response = await fetch('https://api.lanyard.rest/v1/users/399911902211473410', {
+    cache: 'no-cache' // Add this line to ignore cache
+  });
 
-      // Fetch data from another API endpoint using await
-      const response = await fetch('https://api.lanyard.rest/v1/users/399911902211473410');
-      // Check if the response is ok
-      if (response.ok) {
-        // Parse the response as JSON using await
-        const json = await response.json();
-        // Log the JSON data
-        console.log(json);
-        // Send back a success response with status code 200 and JSON data
-        
-        return NextResponse.json(json);
+  // Check if the response is ok
+  if (response.ok) {
+    // Parse the response as JSON using await
+    const json = await response.json();
+    // Log the JSON data
+    console.log(json);
+    // Send back a success response with status code 200 and JSON data
+    return NextResponse.json(json);
+  } else {
+    // Send back an error response with status code and message
+    return NextResponse.json({ msg: 'There was an issue getting user info' });
+  }
+}
 
-      } else {
-        // Send back an error response with status code and message
-        NextResponse.json({ msg: 'There was an issue getting user info' });
-      }
-    } 
+
+
+
+
