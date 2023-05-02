66e531e79b79989d3d143afa4d16878a90dde849
remixor <ergindapaj081@gmail.com>
2023-05-02 19:58:03 +0200

    nice

--- src/app/security/page.tsx
+++ src/app/security/page.tsx
@@ -10,10 +10,12 @@ import {
   ModalHeader,
   ModalBody,
   ModalCloseButton,
+  CircularProgressLabel,
   Button,
   SimpleGrid,
   useDisclosure,
   Image,
+  CircularProgress,
   Center,
   Flex,
   VStack,
@@ -114,144 +116,149 @@ export default function Security() {
               Check out some of my security-related projects below.
             </Text>
           </Box>
-          {/* {isLoading ? (
-            <Loader progress={0} />
-          ) : ( */}
-          <Box px={10}>
-          <Grid templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']} gap={6}>
-    {securityData.map((project, index) => (
-      <GridItem
-        key={index}
-  
-        bgGradient={`linear-gradient(to top right, ${project.severity === "High" ? "#6b3d3a" : project.severity === "Medium" ? "#705b3a" : "#fc1ede"}, rgba(255, 255, 255, 0.1))`}
-        boxShadow="md"
-        borderRadius="lg"
-        overflow="hidden"
-        onClick={() => {
-          setSelectedProject(project);
-          onOpen();
-        }}
-        cursor="pointer"
-        transition="all 0.2s ease-in-out"
-        _hover={{
-          transform: "translateY(-4px)",
-          boxShadow: "lg",
-        }}
-      >
-        <Box p={6}>
-          <Flex alignItems="center" mb={4}>
-            {getIcon(project.icon)}
-            <Text
-              ml={2}
-              fontWeight="semibold"
-              isTruncated
-              title={project.name}
-              color="white"
-            >
-              {project.name}
-            </Text>
+          {isLoading ? (
+            <Flex alignItems="center" justifyContent="center" height="50vh">
+            <CircularProgress isIndeterminate color="#2a9df5" />
           </Flex>
+          ) : (
+            <Box px={10}>
+              <Grid templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']} gap={6}>
+                {securityData.map((project, index) => (
+                  <GridItem
+                    key={index}
 
-          <Box>
-            <Text color="gray.300" fontSize="md">
-              {project.description}
-            </Text>
-          </Box>
-        </Box>
-        <Box py={4} px={6} bg="gray.700">
-          <Flex justifyContent="space-between">
-            <Text
-              fontWeight="semibold"
-              fontSize="md"
-              color="gray.200"
-            >
-              Severity:
-            </Text>
-            <Text
-              fontSize="md"
-              color={
-                project.severity === "High"
-                  ? "red.500"
-                  : project.severity === "Medium"
-                  ? "orange.500"
-                  : "gray.500"
-              }
-            >
-              {project.severity}
-            </Text>
-          </Flex>
-          <Flex justifyContent="space-between" mt={2}>
-            <Text fontWeight="semibold" fontSize="md" color="gray.200">
-              Vulnerability Type:
-            </Text>
-            <Flex alignItems="center">
-              <FontAwesomeIcon icon={faLock} size="sm" color="#000" className="lock-icon" />
-            </Flex>
-          </Flex>
-        </Box>
-      </GridItem>
-    ))}
-  </Grid>
-          </Box>
-          </Box>
+                    bgGradient={`linear-gradient(to top right, ${project.severity === "High" ? "#6b3d3a" : project.severity === "Medium" ? "#705b3a" : "#fc1ede"}, rgba(255, 255, 255, 0.1))`}
+                    boxShadow="md"
+                    borderRadius="lg"
+                    overflow="hidden"
+                    onClick={() => {
+                      setSelectedProject(project);
+                      onOpen();
+                    }}
+                    cursor="pointer"
+                    transition="all 0.2s ease-in-out"
+                    _hover={{
+                      transform: "translateY(-4px)",
+                      boxShadow: "lg",
+                    }}
+                  >
+                    <Box p={6}>
+                      <Flex alignItems="center" mb={4}>
+                        {getIcon(project.icon)}
+                        <Text
+                          ml={2}
+                          fontWeight="semibold"
+                          isTruncated
+                          title={project.name}
+                          color="white"
+                        >
+                          {project.name}
+                        </Text>
+                      </Flex>
 
-          {/* )} */}
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <Box
-                          maxW="600px"
-              borderRadius="md"
-              boxShadow="md"
-            >
-             <ModalContent
-  maxW="600px"
-  bgGradient={`linear(to-b, ${selectedProject?.severity === "High" ? "#F56565" : selectedProject?.severity === "Medium" ? "#ED8936" : "#CBD5E0"}, rgba(0, 0, 0, 0.3))`}
->
-                <ModalHeader
-                  color="gray.800"
-                  fontSize="2xl"
-                  fontWeight="bold"
-                  mb={2}
-                >
-                  {selectedProject?.name}
-                </ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                  <Box mb={4}>
-                    <Image
-                      src={selectedProject?.image}
-                      alt={`Screenshot of ${selectedProject?.name} website`}
-                      objectFit="cover"
-                      maxH="400px"
-                      w="100%"
-                      borderRadius="md"
-                    />
-                  </Box>
-                  <Box mb={4}>
-                    <Text fontSize="lg" color="gray.800">
-                      {selectedProject?.description}
-                    </Text>
-                  </Box>
-                  <Flex justify="flex-end">
-                    <Button
-                      colorScheme="green"
-                      variant="outline"
-                      onClick={() =>
-                        window.open(selectedProject?.link, '_blank')
-                      }
-                    >
-                      Visit Website
-                    </Button>
-                  </Flex>
-                </ModalBody>
-              </ModalContent>
+                      <Box>
+                        <Text color="gray.300" fontSize="md">
+                          {project.description}
+                        </Text>
+                      </Box>
+                    </Box>
+                    <Box py={4} px={6} bg="gray.700">
+                      <Flex justifyContent="space-between">
+                        <Text
+                          fontWeight="semibold"
+                          fontSize="md"
+                          color="gray.200"
+                        >
+                          Severity:
+                        </Text>
+                        <Text
+                          fontSize="md"
+                          color={
+                            project.severity === "High"
+                              ? "red.500"
+                              : project.severity === "Medium"
+                                ? "orange.500"
+                                : "gray.500"
+                          }
+                        >
+                          {project.severity}
+                        </Text>
+                      </Flex>
+                      <Flex justifyContent="space-between" mt={2}>
+                        <Text fontWeight="semibold" fontSize="md" color="gray.200">
+                          Vulnerability Type:
+                        </Text>
+                        <Flex alignItems="center">
+                          <FontAwesomeIcon icon={faLock} size="sm" color="#000" className="lock-icon" />
+                        </Flex>
+                      </Flex>
+                    </Box>
+                  </GridItem>
+                ))}
+              </Grid>
             </Box>
-          </Modal>
+          )}
+          </Box>
+          
+
+        {/* )} */}
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <Box
+            maxW="600px"
+            borderRadius="md"
+            boxShadow="md"
+          >
+            <ModalContent
+              maxW="600px"
+              bgGradient={`linear(to-b, ${selectedProject?.severity === "High" ? "#6b3d3a" : selectedProject?.severity === "Medium" ? "#705b3a" : "#fc1ede"}, rgba(0, 0, 0, 0.3))`}
+            >
+              <ModalHeader
+                color="gray.300"
+                fontSize="2xl"
+                fontWeight="bold"
+                mb={2}
+              >
+                {selectedProject?.name}
+              </ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Box mb={4}>
+                  <Image
+                    src={selectedProject?.image}
+                    alt={`Screenshot of ${selectedProject?.name} website`}
+                    objectFit="cover"
+                    maxH="400px"
+                    w="100%"
+                    borderRadius="md"
+                  />
+                </Box>
+                <Box mb={4}>
+                  <Text fontSize="lg" color="gray.300">
+                    {selectedProject?.description}
+                  </Text>
+                </Box>
+                <Flex justify="flex-end">
+                  <Button
+                    colorScheme="green"
+                    variant="outline"
+                    onClick={() =>
+                      window.open(selectedProject?.link, '_blank')
+                    }
+                  >
+                    Visit Website
+                  </Button>
+                </Flex>
+              </ModalBody>
+            </ModalContent>
+          </Box>
+        </Modal>
 
 
-        
+
       </div>
     </ChakraProvider>
 
   );
+
 }
